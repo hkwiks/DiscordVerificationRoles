2665a0ae6d6362304281afdb0a04ff396c572977
Admin <denisskolnyj03@gmail.com>
2022-01-31 15:01:08 +0200

    Initial commit

---
+++ src/main/java/ua/eizzek/discordverificationroles/DiscordVerification.java
@@ -0,0 +1,17 @@
+package ua.eizzek.discordverificationroles;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiscordVerification extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
